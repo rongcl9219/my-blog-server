1d8ba26f8b5ed6be0cdf53b4287bcb69ecccc4ee
rongcl <963941583@qq.com>
2021-09-24 18:11:56 +0800

    修改gulp打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,56 @@
 const gulp = require('gulp');
 const zip = require('gulp-zip');
+const gulpClean = require('gulp-clean')
 
-// 将项目打包成zip文件，只打包了有后缀的文件，bin文件夹没有打包，需要单独复制到服务器
-gulp.task('zip', function() {
-    return gulp.src(['**/*.*', '!**/node_modules/**/*.*','!dist/**/*.*', '!README.md', '!gulpfile.js', '!table_sql.sql'])
+/**
+ * 打包前清空dist
+ */
+gulp.task('clean', function () {
+    return gulp.src('dist/', {read: false, allowEmpty: true})
+        .pipe(gulpClean());
+})
+
+/**
+ * 打包bin
+ */
+gulp.task('bin', function () {
+    return gulp.src(['bin/**'])
+        .pipe(gulp.dest('dist/bin'));
+})
+
+/**
+ * 打包public
+ */
+gulp.task('public', function () {
+    return gulp.src(['public/**'])
+        .pipe(gulp.dest('dist/public'));
+})
+
+/**
+ * 打包src
+ */
+gulp.task('src', function () {
+    return gulp.src(['src/**'])
+        .pipe(gulp.dest('dist/src'));
+})
+
+/**
+ * 打包views
+ */
+gulp.task('views', function () {
+    return gulp.src(['views/**'])
+        .pipe(gulp.dest('dist/views'));
+})
+
+gulp.task('others', function () {
+    return gulp.src(['app.js', 'package.json', 'package-lock.json'])
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('zip', function () {
+    return gulp.src(['dist/**'])
         .pipe(zip('dist.zip'))
         .pipe(gulp.dest('dist'));
-});
+})
+
+gulp.task('build', gulp.series('clean', gulp.parallel('bin', 'public', 'src', 'views', 'others'), 'zip'))
