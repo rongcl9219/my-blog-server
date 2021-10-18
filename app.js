35a410dc1192302ad1942518a0e33ae3053de59e
rongcl <963941583@qq.om>
2021-10-18 19:54:41 +0800

    test

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.urlencoded({extended: false}));
